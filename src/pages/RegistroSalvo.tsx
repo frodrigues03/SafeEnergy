3b65c2658e09e4dc224bbef626bd13e2a5b4a900
frodrigues03 <168038635+frodrigues03@users.noreply.github.com>
2024-11-22 16:24:20 -0300

    Add files via upload

---
+++ src/pages/RegistroSalvo.tsx
@@ -0,0 +1,17 @@
+// pages/RegistroSalvo.tsx
+
+import React from 'react';
+import { useRouter } from 'next/router';
+
+
+const RegistroSalvo: React.FC = () => {
+  return (
+    <div className="container">
+      <h1>Registro Salvo</h1>
+      <p>O planeta te Agradece</p>
+
+    </div>
+  );
+};
+
+export default RegistroSalvo;
